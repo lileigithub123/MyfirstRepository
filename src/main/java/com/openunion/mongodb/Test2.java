29fb4925ab4f58155be47058e6ad8f800f58c49e
lilei <875390977@qq.com>
2019-03-03 13:41:31 +0800

    111

--- src/main/java/com/openunion/mongodb/Test2.java
+++ src/main/java/com/openunion/mongodb/Test2.java
@@ -7,20 +7,20 @@ import java.util.Date;
 
 public class Test2 {
 
-	public static void main(String[] args) {
-		Calendar c = Calendar.getInstance();
-		Long mill = 0L;
-		try {
-			mill = Long.parseLong("1550472737000");
-		} catch (NumberFormatException e) {
-			e.printStackTrace();
-			throw e;
-		}
-		c.setTimeInMillis(mill);
-		Date date = c.getTime();
-
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		System.out.println(df.format(date));
-	}
+//	public static void main(String[] args) {
+//		Calendar c = Calendar.getInstance();
+//		Long mill = 0L;
+//		try {
+//			mill = Long.parseLong("1550472737000");
+//		} catch (NumberFormatException e) {
+//			e.printStackTrace();
+//			throw e;
+//		}
+//		c.setTimeInMillis(mill);
+//		Date date = c.getTime();
+//
+//		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//		System.out.println(df.format(date));
+//	}
 
 }

32f75b5809eac13ffec9efdbcc1bfa80f7b006f3
lilei <875390977@qq.com>
2019-03-13 14:46:55 +0800

    分支dev第一次提交

--- src/main/java/com/openunion/mongodb/App.java
+++ src/main/java/com/openunion/mongodb/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("hahhahahahahaha");
         //System.out.println("hahhahahahahaha");
+	System.out.println("分支dev增加的代码");
     }
 }
